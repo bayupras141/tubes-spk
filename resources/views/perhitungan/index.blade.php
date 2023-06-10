8e9896d501376a7e609b6a0f09991e33a607dfc8
bayupras141 <aebayu789@gmail.com>
2023-06-10 10:07:30 +0700

    tampilan perhitungan

--- resources/views/perhitungan/index.blade.php
+++ resources/views/perhitungan/index.blade.php
@@ -1,37 +1,52 @@
 @extends('layouts.app')
 @section('content')
-
-{{-- buat 1. Matriks Keputusan, dengan persamaan (1). --}}
 <section id="basic-datatable">
     <div class="row">
     <div class="col-12">
     <div class="card">
         {{-- foreach by kriteria --}}
         <div class="card-header border-bottom p-1">
-            <div class="head-label"><h6 class="mb-0">Matriks Keputusan</h6></div>
+            <div class="head-label"><h6 class="mb-0">Membentuk Matriks Keputusan</h6></div>
         </div>
         <div class="container">
         <table class="data-table table">
             <thead>
                 <tr>
-                    <th>Alternatif</th>
                     <th>C1</th>
                     <th>C2</th>
                     <th>C3</th>
                     <th>C4</th>
+                    <th>C5</th>
+                    <th>C6</th>
                 </tr>
             </thead>
             <tbody>
-                
-                @foreach ($penilaian as $item)
+                <tr class="table-primary">
+                    <td>{{ $minC1 }}</td>
+                    <td>{{ $minC2 }}</td>
+                    <td>{{ $minC3 }}</td>
+                    <td>{{ $minC4 }}</td>
+                    <td>{{ $maxC5 }}</td>
+                    <td>{{ $minC6 }}</td>
+                </tr>
+                @foreach ($alternatif as $item)
                 <tr>
-                    <td>{{ $item->alternatif->kode_alternatif}}</td>
-                    <td>{{ $item->C1 }}</td>
-                    <td>{{ $item->C2 }}</td>
-                    <td>{{ $item->C3 }}</td>
-                    <td>{{ $item->C4 }}</td>
+                    <td>{{ $item->c1 }}</td>
+                    <td>{{ $item->c2 }}</td>
+                    <td>{{ $item->c3 }}</td>
+                    <td>{{ $item->c4 }}</td>
+                    <td>{{ $item->c5 }}</td>
+                    <td>{{ $item->c6 }}</td>
                 </tr>
                 @endforeach
+                <tr class="table-primary">
+                    <td>{{ $sumC1 }}</td>
+                    <td>{{ $sumC2 }}</td>
+                    <td>{{ $sumC3 }}</td>
+                    <td>{{ $sumC4 }}</td>
+                    <td>{{ $sumC5 }}</td>
+                    <td>{{ $sumC6 }}</td>
+                </tr>
             </tbody>
         </table>
         </div>
@@ -40,46 +55,56 @@
     </div>
     </div>
     </div>
+</section>
 
-    {{-- buat Penormalisasian Matriks, dengan persamaan --}}
+<section id="basic-datatable">
     <div class="row">
-        <div class="col-12">
-        <div class="card">
-            {{-- foreach by kriteria --}}
-            <div class="card-header border-bottom p-1">
-                <div class="head-label"><h6 class="mb-0">Normalisasi Matriks</h6></div>
-            </div>
-            <div class="container">
-            <table class="data-table table">
-                <thead>
-                    <tr>
-                        <th>Alternatif</th>
-                        <th>C1</th>
-                        <th>C2</th>
-                        <th>C3</th>
-                        <th>C4</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    @foreach ($penilaian as $item)
-                    <tr>
-                        <td>{{ $item->alternatif->kode_alternatif}}</td>
-                        <td>{{ 1 / $item->C1 }}</td>
-                        <td>{{ 1 / $item->C2 }}</td>
-                        <td>{{ 1 / $item->C3 }}</td>
-                        <td>{{ 1 / $item->C4 }}</td>
-                    </tr>
+    <div class="col-12">
+    <div class="card">
+        {{-- foreach by kriteria --}}
+        <div class="card-header border-bottom p-1">
+            <div class="head-label"><h6 class="mb-0">Normalisasi Matriks Keputusan</h6></div>
+        </div>
+        <div class="container">
+        <table class="data-table table">
+            <thead>
+                <tr>
+                    <th>C1</th>
+                    <th>C2</th>
+                    <th>C3</th>
+                    <th>C4</th>
+                    <th>C5</th>
+                    <th>C6</th>
+                </tr>
+            </thead>
+            <tbody>
+                {{-- tampilkan array matriks multidimensi --}}
+
+                @foreach ($matriks as $item)
+                <tr>
+                    @foreach ($item as $i)
+                    <td>{{ number_format($i, 8) }}</td>
                     @endforeach
-                </tbody>
-            </table>
-            </div>
-        
-            {{-- end foreach --}}
-        </div>
-        </div>
-        </div>
+                </tr>
+                @endforeach
 
+                {{-- tampilkan array sumMatriks --}}
+                <tr class="table-primary">
+                    <td>{{ number_format($sumMatriks[0], 8) }}</td>
+                    <td>{{ number_format($sumMatriks[1], 8) }}</td>
+                    <td>{{ number_format($sumMatriks[2], 8) }}</td>
+                    <td>{{ number_format($sumMatriks[3], 8) }}</td>
+                    <td>{{ number_format($sumMatriks[4], 8) }}</td>
+                    <td>{{ number_format($sumMatriks[5], 8) }}</td>
+                </tr>
 
+            </tbody>
+        </table>
+        </div>
+    
+        {{-- end foreach --}}
+    </div>
+    </div>
+    </div>
 </section>
 @endsection
